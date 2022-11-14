770885566baa66e76b6054d75324ec1cb20d2c35
nasar <nasar@localhost>
2022-11-14 14:28:12 +0530

    Allure repor code added

--- src/main/java/com/aloro/pageobject/LoginPage.java
+++ src/main/java/com/aloro/pageobject/LoginPage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.support.PageFactory;
 import com.aloro.actiondriver.Action;
 import com.aloro.base.BaseClass;
 
+import io.qameta.allure.Step;
+import io.qameta.allure.Stories;
+
 /**
  * @author Nasar
  *
@@ -26,20 +29,24 @@ public class LoginPage extends BaseClass {
 	public LoginPage(){
 		PageFactory.initElements(getDriver(), this);
 	}
-	
+	@Step("Enter the user name: {0} step...")
 	public void enter_user_name(String Username) {
 		action.type(userid, Username);
 	}
+	@Step("Enter the password: {1} step...")
 	public void enter_password(String Password) {
 		action.type(password, Password);
 	}
+	@Step("Click on Login Button step...")
 	public HomePage click_login_button() {
 		action.click(getDriver(), Loginbutton);
 		return new HomePage();	
 	}
+	@Step("Verifying home page displayed or not step...")
 	public Boolean verify_login() {
 	    return action.isDisplayed(getDriver(),verifylogin);
 	}
+	@Step("Verifying Login button displayed or not step...")
 	public boolean verify_web_launch() {
 		return action.isDisplayed(getDriver(), Loginbutton);
 	}
--- src/test/java/com/aloro/testcases/LoginTest.java
+++ src/test/java/com/aloro/testcases/LoginTest.java
@@ -7,15 +7,23 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.aloro.base.BaseClass;
 import com.aloro.dataprovider.DataProviders;
 import com.aloro.pageobject.LoginPage;
+import com.aloro.utility.AllureListener;
 import com.aloro.utility.Log;
 import com.aloro.utility.VideoRecorder;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+@Listeners(AllureListener.class)
 public class LoginTest extends BaseClass{
 
 	private LoginPage loginpage;
@@ -30,7 +38,10 @@ public class LoginTest extends BaseClass{
 	public void tearDown() throws Exception {
 		getDriver().quit();
 	}
-	@Test(priority = 0,groups= {"Smoke","Sanity","Regression"})
+	@Test(priority = 0,groups= {"Smoke","Sanity","Regression"},description = "Verify project launch url test case")
+	@Severity(SeverityLevel.NORMAL)
+	@Description("Test Case Description: Verify Project launch url")
+	@Story("Story Name: To Check project launching url correct or not")
 	public void Verify_Webapp_launch_Tc01() {
 		loginpage=new LoginPage();
 		Log.startTestCase("Login Test Case Execution Start here");
@@ -39,8 +50,10 @@ public class LoginTest extends BaseClass{
 		Assert.assertTrue(launch_verify);
 		Log.endTestCase("Test Case Execution End here");
 	}
-	
-	@Test(priority = 1,groups= {"Smoke","Sanity","Regression"},dataProvider="credentials",dataProviderClass = DataProviders.class)	
+	@Test(priority = 1,groups= {"Smoke","Sanity","Regression"},dataProvider="credentials",dataProviderClass = DataProviders.class,description = "Verify Login test case")
+	@Severity(SeverityLevel.BLOCKER)
+	@Description("Test Case Description: Verify Login functionality test case")
+	@Story("Story Name: To Check login functionality of the web app")
 	public void Verify_Login_Tc02(String Username,String Password) throws Throwable {
 		loginpage=new LoginPage();
 	    Log.startTestCase("Login Test Case Execution Start here");
